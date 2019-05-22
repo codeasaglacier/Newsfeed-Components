df42ed23d3a23390417a8d4a363331246cd7b889
codeasaglacier <remingtonshock@gmail.com>
2019-05-22 14:44:26 -0800

    set up article constructor, added new articles

--- Article/Article.js
+++ Article/Article.js
@@ -2,19 +2,20 @@
 
 class Article {
   constructor(domElement) {
-    // assign this.domElement to the passed in domElement
-    this.domElement;
+    // assign this.domElement to the passed in 
+    console.dir(domElement);
+    this.domElement = domElement;
     // create a reference to the ".expandButton" class. 
-    this.expandButton;
+    this.expandButton = domElement.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
+    this.expandButton.textContent = 'expand';
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    
+    this.expandButton.addEventListener('click', ()=> this.expandArticle());
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-
+    this.domElement.classList.toggle('article-open');
   }
 }
 
@@ -26,4 +27,11 @@ class Article {
 
 */
 
-let articles;
+const articles = document.querySelectorAll('.article');
+// iterate over NodeList articles
+// call each element article
+// passing each article into Article class
+// instantiate new keyword
+articles.forEach((article) => {
+new Article(article);
+});
